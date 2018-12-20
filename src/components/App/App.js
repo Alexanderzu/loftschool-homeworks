5d7d8582e8c6e4c95e17142d34c18fe3190fcaee
Alexanderzu <artminor@yandex.ru>
2018-12-20 12:04:36 +0300

    hw-1

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+export default class App extends React.Component  {
+    render() {
+        return(
+            <div className="App">
+                <p className="description">
+                    Миру-мир, студентам - beer.
+                </p>
+            </div>
+        )
+    }
+}
